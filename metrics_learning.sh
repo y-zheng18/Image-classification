a81cb5dd0df53a313b1b60ccb9c8dadb3d2fd08e
y-zheng18 <y-zheng18@mails.tsinghua.edu.cn>
2020-12-09 10:00:18 +0800

    training on all data

--- metrics_learning.sh
+++ metrics_learning.sh
@@ -1,7 +1,7 @@
 python metrics_learning.py --data_type fine --gpu 0 \
 --model wide_resnet --layers 4 4 4 --wide_factor 20 \
 --lambda_triplet 1 --lambda_rec 1 --lambda_cls 1 \
---bs 128 --lr_policy multi-step --lr_steps 2 4 6 --epoch 10 --embedding_size 512 --use_triplet --lr 0.005 \
---epoch_resume 0 --load_model_dir chkpoints/wide_resnet_SGD_fine.pth --use_all_data --fix_backbone
+--bs 32 --lr_policy multi-step --lr_steps 10 20 30 --epoch 40 --embedding_size 256 --use_triplet --lr 0.001 \
+--epoch_resume 0 --load_model_dir chkpoints/wide_resnet_SGD_fine.pth --use_all_data
 #--load_autoencoder_dir chkpoints/autoencoder_512.pth \
 #--load_optim_dir chkpoints/optim_autoencoder_512.pth --use_all_data
---
+++ train_wideres_coarse.sh
@@ -0,0 +1,3 @@
+python train.py --model wide_resnet --gpu 0 --bs 64 --lr_policy multi-step --lr_steps 60 120 160 --data_type coarse \
+--layers 4 4 4 --wide_factor 20 \
+--lr 0.1 --dropout_rate 0
