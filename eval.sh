00f0675ade3e5cfe7f2ec16d57755725c4716868
y-zheng18 <y-zheng18@mails.tsinghua.edu.cn>
2020-12-14 00:11:50 +0800

    metrics learning

--- eval.sh
+++ eval.sh
@@ -1,2 +1,2 @@
-python evaluation.py --data_type coarse --gpu 0 \
---load_model_dir chkpoints/wideresnet_pretrained_SGD_coarse.pth --model wideresnet_pretrained --pretrained --layers 6 6 6 --wide_factor 10
+python evaluation.py --data_type fine --gpu 0 \
+--load_model_dir chkpoints/resnet_pretrained_SGD_fine.pth --model resnet_pretrained --pretrained --layers 6 6 6 --wide_factor 10
